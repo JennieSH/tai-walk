9ccf679e0c2ef7b5301618f6ecd309d49cd5e4bb
jennie.shao <m65pjj@gmail.com>
2021-11-26 21:51:54 +0800

    style: add transition

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,6 +2,7 @@ import { css } from "@emotion/react";
 import emotionNormalize from "emotion-normalize";
 import { theme, ThemeType } from "./theme";
 import "./fonts.css";
+import "./transition.css";
 
 const generateGlobalStyle = (theme: ThemeType) => {
   return css`
@@ -13,6 +14,20 @@ const generateGlobalStyle = (theme: ThemeType) => {
       background: ${theme.color.white};
       min-height: 100%;
       font-family: ${theme.fontFamily["noto-sans"]}, Helvetica, Arial, sans-serif;
+    }
+
+    ul {
+      margin-block-start: 0;
+      margin-block-end: 0;
+      padding-inline-start: 0;
+    }
+
+    li {
+      list-style: none;
+    }
+
+    a {
+      text-decoration: none;
     }
   `;
 };
