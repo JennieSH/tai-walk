448366026cf5434230e36cb9d46bc5603824da20
jennie.shao <m65pjj@gmail.com>
2021-12-01 08:25:14 +0800

    feat: add Error404 page

---
+++ src/pages/Error404/index.tsx
@@ -0,0 +1,7 @@
+import { StyledError404 } from "./styles";
+
+const Error404 = () => {
+  return <StyledError404 className="container">Page Not Found</StyledError404>;
+};
+
+export default Error404;
---
+++ src/pages/Error404/styles.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const StyledError404 = styled.main`
+  margin-top: 100px;
+  text-align: center;
+`;
