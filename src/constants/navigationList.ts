f03e4f7ab764b91a470b9c10e6eaecad9b16fd6c
jennie.shao <m65pjj@gmail.com>
2021-12-01 08:29:19 +0800

    feat<navigationList>: replace string path with enum route

--- src/constants/navigationList.ts
+++ src/constants/navigationList.ts
@@ -1,6 +1,8 @@
+import Routes from "@/types/routes";
+
 const navigationList = [
-  { path: "/scenic-spot", text: "探索景點" },
-  { path: "/activity", text: "節慶活動" },
-  { path: "/restaurant", text: "品嚐美食" }
+  { path: Routes.SCENIC_SPOT, text: "探索景點" },
+  { path: Routes.ACTIVITY, text: "節慶活動" },
+  { path: Routes.RESTAURANT, text: "品嚐美食" }
 ];
 export default navigationList;
