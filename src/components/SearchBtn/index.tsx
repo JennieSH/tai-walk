2a6bac7ccd838f0f83cb7dae7ac51e98a2bb7b89
jennie.shao <m65pjj@gmail.com>
2021-12-01 08:31:08 +0800

    feat<SearchBtn>: add className props

--- src/components/SearchBtn/index.tsx
+++ src/components/SearchBtn/index.tsx
@@ -2,9 +2,9 @@ import { StyledSearchBtn } from "./styles";
 import { SearchBtnProps } from "./types";
 import Icon from "@/components/Icon";
 
-const SearchBtn = ({ clickHandler }: SearchBtnProps) => {
+const SearchBtn = ({ className, clickHandler }: SearchBtnProps) => {
   return (
-    <StyledSearchBtn onClick={clickHandler}>
+    <StyledSearchBtn className={className} onClick={clickHandler}>
       <Icon name="search" />
       <span>搜尋</span>
     </StyledSearchBtn>
