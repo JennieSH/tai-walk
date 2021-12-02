7c47e33627dda3c624515d5f802742f3a9e8bab1
jennie.shao <m65pjj@gmail.com>
2021-12-02 15:15:58 +0800

    chore: add Google Map Key

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_PTX_KEY: string;
   readonly VITE_APP_PTX_ID: string;
   readonly VITE_APP_BASE_URL: string;
+  readonly VITE_APP_GOOGLE_MAP_KEY: string;
+  readonly VITE_APP_GOOGLE_MAP_URL: string;
 }
 
 interface ImportMeta {
