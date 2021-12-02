79e24be6d62442bc2cad4f10082bcc20fb6b169e
jennie.shao <m65pjj@gmail.com>
2021-12-02 12:03:37 +0800

    refactor<Search>: use useTourismApi to fetch data instead of tourismApi

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -6,65 +6,80 @@ import Input from "@/components/Input";
 import SearchBtn from "@/components/SearchBtn";
 import Category from "@/components/Category";
 import SearchList from "@/components/SearchList";
-// import Calendar from "@/components/Calendar";
+import Calendar from "@/components/Calendar";
 import Dropdown from "@/components/Dropdown";
-import { CityMap, CityTextMap } from "@/types/city";
-// import { Category as CategoryEnum } from "@/types/category";
+import Breadcrumb from "@/components/Breadcrumb";
+import useQuery from "@/hooks/useQuery";
+import { useTourismApi, SearchType } from "@/hooks/useTourismApi";
+import { Category as CategoryEnum } from "@/types/category";
+import { City, CityMap, CityTextMap } from "@/types/city";
 import type { CityText } from "@/types/city";
 import type { CategoryType } from "@/types/category";
-import type { ScenicSpotCard as ScenicSpotCardType } from "@/types/scenic-spot";
-import useQuery from "@/hooks/useQuery";
-import tourismApi from "@/api/tourism";
+import type {
+  ActivityCard as ActivityCardType,
+  ScenicSpotCard as ScenicSpotCardType,
+  RestaurantCard as RestaurantCardType
+} from "@/types/tourism";
 import { cityTextList } from "@/constants/city";
 import { categoryPlaceholder } from "@/constants/categoryList";
+import { formatTime } from "@/utils/format";
 
 type SearchParams = {
   category: CategoryType;
 };
-
 type QueryCityType = keyof typeof CityTextMap;
+type SearchDataType = ActivityCardType | ScenicSpotCardType | RestaurantCardType;
 
 const Search = () => {
   const [cityInput, setCityInput] = useState<CityText | null>(null);
+  const [dateInput, setDateInput] = useState<Date | null>(null);
   const [keywordInput, setKeywordInput] = useState("");
-  const [searchData, setSearchData] = useState<ScenicSpotCardType[] | null>(null);
+  const [searchData, setSearchData] = useState<SearchDataType[] | null>(null);
   const { category } = useParams<SearchParams>();
-  const { query, routerPush } = useQuery(["keyword", "city"]);
+  const { query, routerPush } = useQuery(["city", "date", "keyword"]);
+  const { fetchTourismData } = useTourismApi();
 
   const onSearch = () => {
     const target: { [key: string]: string | null } = {};
     target["keyword"] = keywordInput;
     target["city"] = cityInput && CityMap[cityInput];
+    target["date"] = formatTime(dateInput, "YYYY-MM-DD");
     routerPush(target);
   };
 
-  const fetchSearchData = async (filter: string[]) => {
-    const data = await tourismApi.getScenicSpot<ScenicSpotCardType[]>({
-      $select: "ID,Name,Address,Picture,City",
-      $filter: filter.join(" and "),
-      $orderBy: "UpdateTime desc"
+  const fetchSearchData = async (
+    city: City | null,
+    date: string | null,
+    keyword: string | null
+  ) => {
+    const data = await fetchTourismData<SearchDataType>({
+      page: SearchType.MULTIPLE,
+      category: category,
+      city: city,
+      keyword: keyword,
+      date: date
     });
+
     setSearchData(data);
   };
 
   useEffect(() => {
     setCityInput(query.city ? CityTextMap[query.city as QueryCityType] : null);
+    setDateInput(query.date ? new Date(query.date) : null);
     setKeywordInput(query.keyword || "");
     setSearchData(null);
   }, [category]);
 
   useEffect(() => {
-    const { city, keyword } = query;
-    if (city || keyword) {
-      const filterConditions = ["Picture/PictureUrl1 ne null and City ne null"];
-      if (city) filterConditions.push(`City eq '${CityTextMap[city as QueryCityType]}'`);
-      if (keyword) filterConditions.push(`contains(Description, '${keyword}')`);
-      fetchSearchData(filterConditions);
+    const { city, date, keyword } = query;
+    if (city || date || keyword) {
+      fetchSearchData(city as City, date, keyword);
     }
-  }, [query.city, query.keyword]);
+  }, [query.city, query.date, query.keyword]);
 
   return (
     <StyledSearch className="container">
+      <Breadcrumb category={category} />
       <div className="search-block">
         <Dropdown
           className="search-dropdown"
@@ -72,7 +87,9 @@ const Search = () => {
           value={cityInput ? cityInput : "全部縣市"}
           setValue={setCityInput}
         />
-        {/* {category === CategoryEnum.ACTIVITY && <Calendar className="search-calendar" />} */}
+        {category === CategoryEnum.ACTIVITY && (
+          <Calendar className="search-calendar" date={dateInput} changeHandler={setDateInput} />
+        )}
         <Input
           className="search-input"
           placeholder={categoryPlaceholder[category]}
